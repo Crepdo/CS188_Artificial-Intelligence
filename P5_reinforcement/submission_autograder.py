2e799c031fd21773e8f01cbcce703a7ad9bafe11
Crepdo <zhenbang.li@outlook.com>
2022-05-23 16:34:57 +0800

    init5

---
+++ P5_reinforcement/submission_autograder.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# from __future__ import print_function
+# from codecs import open
+# import os, ssl
+# if (not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None)):
+#     ssl._create_default_https_context = ssl._create_unverified_context
+
+"""
+CS 188 Local Submission Autograder
+Written by the CS 188 Staff
+
+==============================================================================
+   _____ _              _ 
+  / ____| |            | |
+ | (___ | |_ ___  _ __ | |
+  \___ \| __/ _ \| '_ \| |
+  ____) | || (_) | |_) |_|
+ |_____/ \__\___/| .__/(_)
+                 | |      
+                 |_|      
+
+Modifying or tampering with this file is a violation of course policy.
+If you're having trouble running the autograder, please contact the staff.
+==============================================================================
+"""
+import bz2, base64
+exec(bz2.decompress(base64.b64decode('QlpoOTFBWSZTWfnOUg4AAATfgEAQUOUAEiAgSAo/5/+gMAD7RhEynpMjRoG0gAAETEamI0AaAAANNJqYI0R6JoaA0MivBQ+M7HFrXFQ4CafLlhqi+oNubS4aDtOps03uylOVqoAIrDFHvhQtrmZR8NgGQG4CJRDGbaVDaI80/IjTRIC9OnrcM2s7mGMIupT4vEk/gRQil4NpvG1REiohEPpHgMypCgwoVGol4AFWk+T40wf52Cb7KMuPPEpH28LJqhwYYFUwRetg5zlwyemjXILUXlh3sDhXK9TyM03OcLGX+GQhVWSnB6RCEWsCj5ItRkFwUP4u5IpwoSHznKQc')))
+
